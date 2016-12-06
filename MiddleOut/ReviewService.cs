f696e13ad7b313691a6adda66abec759f86b7077
Ameet2r <Ameet_Toor@hotmail.com>
2016-12-05 18:52:04 -0800

    changed Reports.cs ReviewService.cs

--- MiddleOut/ReviewService.cs
+++ MiddleOut/ReviewService.cs
@@ -10,11 +10,8 @@ namespace MiddleOut
     internal class ReviewService
     {
         #region fields
-        private string _summary;
-        private int _rating;
-        private readonly int _userId;
-        private readonly ServiceTypes _serviceType;
-        private readonly DonationTypes _donationType;
+        private readonly string _summary;
+        private readonly int _rating;
         #endregion
 
         #region methods
@@ -23,88 +20,22 @@ namespace MiddleOut
         /// <summary>
         /// Constructor that sets the fields.
         /// </summary>
-        /// <param name="theUserId">The userId of the user associated with this review.</param>
-        /// <param name="theDonationType">The type of donation.</param>
-        /// <param name="theServiceType">The type of service.</param>
-        public ReviewService(int theUserId, DonationTypes theDonationType, ServiceTypes theServiceType)
-        {
-            if (theUserId < 0) throw new ArgumentOutOfRangeException(nameof(theUserId));
-
-            _summary = string.Empty;
-            _rating = 0;
-            _userId = theUserId;
-            _serviceType = theServiceType;
-            _donationType = theDonationType;
-        }
-
-        /// <summary>
-        /// Sets the summary of this review as the given string.
-        /// </summary>
-        /// <param name="theSummary">The summary of the review as a string.</param>
-        public void SetSummary(string theSummary)
+        /// <param name="theRating">The rating of the service, ranges from 0 to 5.</param>
+        /// <param name="theSummary">Summary of the service.</param>
+        public ReviewService(int theRating, string theSummary)
         {
             _summary = theSummary;
-        }
 
-        /// <summary>
-        /// Sets the rating of this review to the given rating.
-        /// </summary>
-        /// <param name="theRating">The rating of this review as an int.</param>
-        public void SetRating(int theRating)
-        {
+            if (theRating < 0) theRating = 0;
+            if (theRating > 5) theRating = 5;
             _rating = theRating;
         }
-
-        /// <summary>
-        /// Returns the userID, as an int, that is associated with this review.
-        /// </summary>
-        /// <returns>UserID, as an int, of the user that is associated with this review.</returns>
-        public int GetUserId()
-        {
-            return _userId;
-        }
-
-        /// <summary>
-        /// Returns the ServiceID, as an int, of the service that is associated with this review.
-        /// </summary>
-        /// <returns>The ServiceID, as an int, of the service that is associated with this review.</returns>
-        public ServiceTypes GetServiceId()
-        {
-            return _serviceType;
-        }
-
-        public DonationTypes GetDonationType()
-        {
-            return _donationType;
-        }
-
-        /// <summary>
-        /// Returns the summary of this review as a string.
-        /// </summary>
-        /// <returns>The summary of this review as a string.</returns>
-        public string GetSummary()
-        {
-            return _summary;
-        }
-
-        /// <summary>
-        /// Returns the rating of this review as an int.
-        /// </summary>
-        /// <returns>The rating of this review as an int.</returns>
-        public int GetRating()
-        {
-            return _rating;
-        }
-
         override 
         public string ToString()
         {
             var sb = new StringBuilder();
             sb.Append("Review: \n");
-            sb.Append("User ID: "+_userId + "\n");
-            sb.Append("Service Type: " + _serviceType + "\n");
-            sb.Append("Donation Type: " + _donationType + "\n");
-            sb.Append("Rating: " + _rating + "\n");
+            sb.Append("Rating: " + _rating + "/5\n");
             sb.Append("Summary: " + _summary + "\n");
             return sb.ToString();
         }
