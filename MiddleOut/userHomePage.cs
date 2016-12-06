30c7a0af76f3fdba29071ce83c91dbd92764afd8
LJ-S-H <lovejitshari@gmail.com>
2016-12-06 14:26:00 -0800

    Created userHomePage shell, temporary for now

--- MiddleOut/userHomePage.cs
+++ MiddleOut/userHomePage.cs
@@ -21,5 +21,37 @@ namespace MiddleOut
         {
 
         }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
     }
 }
