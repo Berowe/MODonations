7059e6698891e96a54341b0fc37da6f7c46b4c52
KaranToor <toork@uw.edu>
2016-12-07 19:12:21 -0800

    Saving my changes to forms

--- MiddleOut/InformationPage.xaml.cs
+++ MiddleOut/InformationPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using FirstFloor.ModernUI.Windows.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,40 @@ namespace MiddleOut
         {
             IInputElement target = NavigationHelper.FindFrame("ListPage1", this);
             NavigationCommands.GoToPage.Execute("/ThankYou.xaml", target);
+        }
+
+        private void richTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //Process.Start(@"c:\"); @Karan
+
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                String[] temp = filename.Split('\\');
+                textBlock1.Text = temp[temp.Length - 1];
+            }
+
         }
     }
 }
