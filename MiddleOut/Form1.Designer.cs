092b09380100606f111790f685740bae334e65f8
KaranToor <toork@uw.edu>
2016-12-13 14:57:10 -0800

    Fixed another form bug - unneeded methods

--- MiddleOut/Form1.Designer.cs
+++ MiddleOut/Form1.Designer.cs
@@ -48,10 +48,9 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(139)))), ((int)(((byte)(212)))));
             this.panel2.Controls.Add(this.passwordBox);
-            this.panel2.Location = new System.Drawing.Point(110, 264);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Location = new System.Drawing.Point(165, 406);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(263, 46);
+            this.panel2.Size = new System.Drawing.Size(394, 71);
             this.panel2.TabIndex = 1;
             // 
             // passwordBox
@@ -60,11 +59,10 @@
             this.passwordBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.passwordBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.passwordBox.ForeColor = System.Drawing.Color.White;
-            this.passwordBox.Location = new System.Drawing.Point(23, 11);
-            this.passwordBox.Margin = new System.Windows.Forms.Padding(2);
+            this.passwordBox.Location = new System.Drawing.Point(34, 17);
             this.passwordBox.Name = "passwordBox";
             this.passwordBox.PasswordChar = '•';
-            this.passwordBox.Size = new System.Drawing.Size(219, 19);
+            this.passwordBox.Size = new System.Drawing.Size(328, 28);
             this.passwordBox.TabIndex = 0;
             this.passwordBox.TextChanged += new System.EventHandler(this.passwordBox_TextChanged);
             // 
@@ -75,10 +73,9 @@
             this.signInSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.signInSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.signInSubmit.ForeColor = System.Drawing.Color.White;
-            this.signInSubmit.Location = new System.Drawing.Point(110, 352);
-            this.signInSubmit.Margin = new System.Windows.Forms.Padding(2);
+            this.signInSubmit.Location = new System.Drawing.Point(165, 542);
             this.signInSubmit.Name = "signInSubmit";
-            this.signInSubmit.Size = new System.Drawing.Size(263, 42);
+            this.signInSubmit.Size = new System.Drawing.Size(394, 65);
             this.signInSubmit.TabIndex = 2;
             this.signInSubmit.Text = "Sign in";
             this.signInSubmit.UseVisualStyleBackColor = false;
@@ -88,10 +85,9 @@
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(139)))), ((int)(((byte)(212)))));
             this.panel3.Controls.Add(this.emailBox);
-            this.panel3.Location = new System.Drawing.Point(110, 177);
-            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Location = new System.Drawing.Point(165, 272);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(263, 46);
+            this.panel3.Size = new System.Drawing.Size(394, 71);
             this.panel3.TabIndex = 2;
             // 
             // emailBox
@@ -100,10 +96,9 @@
             this.emailBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.emailBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.emailBox.ForeColor = System.Drawing.Color.White;
-            this.emailBox.Location = new System.Drawing.Point(23, 14);
-            this.emailBox.Margin = new System.Windows.Forms.Padding(2);
+            this.emailBox.Location = new System.Drawing.Point(34, 22);
             this.emailBox.Name = "emailBox";
-            this.emailBox.Size = new System.Drawing.Size(219, 19);
+            this.emailBox.Size = new System.Drawing.Size(328, 28);
             this.emailBox.TabIndex = 0;
             this.emailBox.TextChanged += new System.EventHandler(this.emailBox_TextChanged);
             this.emailBox.Enter += new System.EventHandler(this.emailBox_Enter);
@@ -116,10 +111,9 @@
             this.signUpButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.signUpButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.signUpButton.ForeColor = System.Drawing.Color.White;
-            this.signUpButton.Location = new System.Drawing.Point(377, 18);
-            this.signUpButton.Margin = new System.Windows.Forms.Padding(2);
+            this.signUpButton.Location = new System.Drawing.Point(566, 28);
             this.signUpButton.Name = "signUpButton";
-            this.signUpButton.Size = new System.Drawing.Size(93, 36);
+            this.signUpButton.Size = new System.Drawing.Size(140, 55);
             this.signUpButton.TabIndex = 3;
             this.signUpButton.Text = "Sign up";
             this.signUpButton.UseVisualStyleBackColor = false;
@@ -132,10 +126,9 @@
             this.aboutButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.aboutButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.aboutButton.ForeColor = System.Drawing.Color.White;
-            this.aboutButton.Location = new System.Drawing.Point(21, 18);
-            this.aboutButton.Margin = new System.Windows.Forms.Padding(2);
+            this.aboutButton.Location = new System.Drawing.Point(32, 28);
             this.aboutButton.Name = "aboutButton";
-            this.aboutButton.Size = new System.Drawing.Size(93, 36);
+            this.aboutButton.Size = new System.Drawing.Size(140, 55);
             this.aboutButton.TabIndex = 4;
             this.aboutButton.Text = "About";
             this.aboutButton.UseVisualStyleBackColor = false;
@@ -146,28 +139,27 @@
             this.errorTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.errorTextBox.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.errorTextBox.ForeColor = System.Drawing.Color.White;
-            this.errorTextBox.Location = new System.Drawing.Point(110, 139);
+            this.errorTextBox.Location = new System.Drawing.Point(165, 214);
+            this.errorTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.errorTextBox.Name = "errorTextBox";
             this.errorTextBox.ReadOnly = true;
-            this.errorTextBox.Size = new System.Drawing.Size(263, 13);
+            this.errorTextBox.Size = new System.Drawing.Size(394, 19);
             this.errorTextBox.TabIndex = 5;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(163)))), ((int)(((byte)(248)))));
-            this.ClientSize = new System.Drawing.Size(488, 470);
+            this.ClientSize = new System.Drawing.Size(732, 723);
             this.Controls.Add(this.errorTextBox);
             this.Controls.Add(this.aboutButton);
             this.Controls.Add(this.signUpButton);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.signInSubmit);
             this.Controls.Add(this.panel2);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Login";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
