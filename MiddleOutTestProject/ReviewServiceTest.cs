6683dba46259569bbe180509be28b2aca2eb9073
Ameet2r <Ameet_Toor@Hotmail.com>
2016-12-13 17:23:55 -0800

    added test classes

---
+++ MiddleOutTestProject/ReviewServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MiddleOutTestProject
+{
+    [TestClass]
+    public class ReviewServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
