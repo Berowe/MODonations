92f2659304ea78db18178ed67807d264dddcc0b9
Ameet2r <Ameet_Toor@Hotmail.com>
2016-12-14 13:13:05 -0800

    removed about button on Form1.Designer.cs

--- MiddleOut/Form1.Designer.cs
+++ MiddleOut/Form1.Designer.cs
@@ -38,7 +38,6 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.emailBox = new System.Windows.Forms.MaskedTextBox();
             this.signUpButton = new System.Windows.Forms.Button();
-            this.aboutButton = new System.Windows.Forms.Button();
             this.errorTextBox = new System.Windows.Forms.TextBox();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -119,20 +118,6 @@
             this.signUpButton.UseVisualStyleBackColor = false;
             this.signUpButton.Click += new System.EventHandler(this.signUpButton_Click);
             // 
-            // aboutButton
-            // 
-            this.aboutButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(139)))), ((int)(((byte)(212)))));
-            this.aboutButton.FlatAppearance.BorderSize = 0;
-            this.aboutButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.aboutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.aboutButton.ForeColor = System.Drawing.Color.White;
-            this.aboutButton.Location = new System.Drawing.Point(32, 28);
-            this.aboutButton.Name = "aboutButton";
-            this.aboutButton.Size = new System.Drawing.Size(140, 55);
-            this.aboutButton.TabIndex = 4;
-            this.aboutButton.Text = "About";
-            this.aboutButton.UseVisualStyleBackColor = false;
-            // 
             // errorTextBox
             // 
             this.errorTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(163)))), ((int)(((byte)(248)))));
@@ -153,7 +138,6 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(163)))), ((int)(((byte)(248)))));
             this.ClientSize = new System.Drawing.Size(732, 723);
             this.Controls.Add(this.errorTextBox);
-            this.Controls.Add(this.aboutButton);
             this.Controls.Add(this.signUpButton);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.signInSubmit);
@@ -177,7 +161,6 @@
         private System.Windows.Forms.MaskedTextBox passwordBox;
         private System.Windows.Forms.MaskedTextBox emailBox;
         private System.Windows.Forms.Button signUpButton;
-        private System.Windows.Forms.Button aboutButton;
         private System.Windows.Forms.TextBox errorTextBox;
     }
 }
