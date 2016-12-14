91fb8d1a58860bb6a470c8b4b71f0023e1ec1d9c
KaranToor <toork@uw.edu>
2016-12-13 20:25:15 -0800

    Dijkstra's alg working!!!!

--- MiddleOut/MapPage.xaml.cs
+++ MiddleOut/MapPage.xaml.cs
@@ -24,6 +24,10 @@ namespace MiddleOut
         private List<Location> myLocations;
         private List<Street> myStreets;
 
+        /// <summary>
+        /// Initialize the MapPage with dummy locations and streets. 
+        /// @Author Karanbir 
+        /// </summary>
         public MapPage()
         {
             InitializeComponent();
@@ -40,35 +44,140 @@ namespace MiddleOut
             myStreets.Add(new Street(5, myLocations[2], myLocations[3]));
             myStreets.Add(new Street(4, myLocations[1], myLocations[3]));
             myStreets.Add(new Street(7, myLocations[1], myLocations[2]));
+
+            List<Street> deleteMe = Dijkstra(myLocations[0], myLocations[3]);
+            Console.WriteLine("Test complete");
         }
 
+        /// <summary>
+        /// Finds the shortest path between two locations! AWESOME!
+        /// @Author Karanbir 
+        /// </summary>
+        /// <param name="theStart">The beginning location.</param>
+        /// <param name="theEnd">The ending location.</param>
+        /// <returns>A list of streets to draw for the shortest path.</returns>
         public List<Street> Dijkstra(Location theStart, Location theEnd)
         {
             List<Street> shortestPath = new List<Street>();
             Dictionary<Location, int> buildPaths = new Dictionary<Location, int>();
-            buildPaths.Add(theStart, 0);
-            
-            bool foundEnd = false;
-            int count = 0;
-            while (!foundEnd) {
-                //List<Street> neighbors = getNeighbors(buildPaths[count]);
+            Dictionary<Location, Location> previousSave = new Dictionary<Location, Location>();
+            List<Location> unvisited = new List<Location>();
+
+            buildPaths.Add(theStart, 0); // 0 for start point 
+
+            // Initialize 
+            foreach (Location loc in myLocations)
+            {
+                if (!loc.Equals(theStart))
+                {
+                    buildPaths.Add(loc, -1); // -1 means infinity
+                    
+                }
+                unvisited.Add(loc);
+
+            }
+
+            while (unvisited.Count != 0)
+            {
+                Location currentLoc = findMin(buildPaths, unvisited);
+                unvisited.Remove(currentLoc);
+                if (currentLoc.Equals(theEnd))
+                {
+                    break; 
+                }
+
+                List<Location> neighbors = getNeighbors(currentLoc);
+                foreach (Location loc in neighbors)
+                {
+                    int currentLocInt;
+                    int neighborInt;
+                    if (findStreet(currentLoc, loc) != null)
+                    {
+                        int alternativeInt = findStreet(currentLoc, loc).myWeight;
+                        buildPaths.TryGetValue(currentLoc, out currentLocInt);
+                        buildPaths.TryGetValue(loc, out neighborInt);
+                        if ((neighborInt < 0) || ((alternativeInt + currentLocInt) < neighborInt))
+                        {
+                            if (buildPaths.ContainsKey(loc))
+                            {
+                                buildPaths.Remove(loc);
+                            }
+                            if (previousSave.ContainsKey(loc))
+                            {
+                                previousSave.Remove(loc);
+                            }
+                            buildPaths.Add(loc, (alternativeInt + currentLocInt));
+                            previousSave.Add(loc, currentLoc);
+                        }
+                    }
+                }
+            }
+            Location retrackLoc;
+            Location firstLoc = theEnd;
+            while (previousSave.TryGetValue(firstLoc, out retrackLoc))
+            {
+                shortestPath.Add(findStreet(retrackLoc, firstLoc));
+                firstLoc = retrackLoc;
             }
             return shortestPath;
         }
 
-        public List<Street> getNeighbors(Location theStart)
+        /// <summary>
+        /// Finds the street based on two locations. 
+        /// </summary>
+        /// <param name="theA">The first location.</param>
+        /// <param name="theB">The second location.</param>
+        /// <returns>The street that links these two locations, otherwise null.</returns>
+        public Street findStreet(Location theA, Location theB)
         {
-            List<Street> neighbors = new List<Street>();
+            foreach (Street st in myStreets)
+            {
+                if ((st.myA.Equals(theA) || st.myB.Equals(theA)) && 
+                    (st.myA.Equals(theB) || st.myB.Equals(theB))) {
+                    return st;
+                }
+            }
+            return null;
+        }
+
+        public Location findMin (Dictionary<Location, int> theDic, List<Location> theUnvisited)
+        {
+            Location toReturn = null;
+            foreach (Location loc in theDic.Keys)
+            {
+                if (toReturn == null && theUnvisited.Contains(loc))
+                {
+                    toReturn = loc;
+                } else if (theUnvisited.Contains(loc))
+                {
+                    int newLocInt;
+                    int toReturnInt;
+                    theDic.TryGetValue(loc, out newLocInt);
+                    theDic.TryGetValue(toReturn, out toReturnInt);
+                    if (newLocInt < toReturnInt && newLocInt >= 0 && theUnvisited.Contains(loc))
+                    {
+                        toReturn = loc;
+                    }
+                }
+            }
+
+            theUnvisited.Remove(toReturn);
+            return toReturn;
+        }
+
+        public List<Location> getNeighbors(Location theStart)
+        {
+            List<Location> neighbors = new List<Location>();
             for (int i = 0; i < myStreets.Count; i ++)
             {
                 Street temp = myStreets[i];
                 if (temp.myA.Equals(theStart))
                 {
-                    neighbors.Add(temp);
+                    neighbors.Add(temp.myB);
                 }
                 else if (temp.myB.Equals(theStart))
                 {
-                    neighbors.Add(temp);
+                    neighbors.Add(temp.myA);
                 }
             }
             return neighbors;
