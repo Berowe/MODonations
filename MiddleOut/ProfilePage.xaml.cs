50e515d8bb74baa4d075a4f378b17b670af3d9f9
LJ-S-H <lovejitshari@gmail.com>
2016-12-14 16:05:43 -0800

    Adding a profile page

---
+++ MiddleOut/ProfilePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MiddleOut
+{
+    /// <summary>
+    /// Interaction logic for ProfilePage.xaml
+    /// </summary>
+    public partial class ProfilePage : UserControl
+    {
+        
+        public ProfilePage()
+        {
+            InitializeComponent();
+            generateReportsOnScreen();
+        }
+        
+        private void generateReportsOnScreen()
+        {
+            MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
+            User user = mainWindow.getUser();
+            
+            Reports r = new Reports(user.getServices());
+            textBlock.Text= r.GetReport();
+            Console.WriteLine(r.GetReport());
+        }
+    }
+}
