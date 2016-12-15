c14408795f371f7bd065aa87ef19ba45ed28b118
KaranToor <toork@uw.edu>
2016-12-14 21:36:11 -0800

    fixed unit test

--- MiddleOutTestProject/DijkstraAlgTest.cs
+++ MiddleOutTestProject/DijkstraAlgTest.cs
@@ -8,13 +8,30 @@ namespace MiddleOutTestProject
     [TestClass]
     public class DijkstraAlgTest
     {
+        List<Location> myLocations = new List<Location>();
+        List<Street> myStreets = new List<Street>();
+
         [TestMethod]
         public void Dijkstra_Alg_Test()
         {
-            
+            myLocations = new List<Location>();
+            myLocations.Add(new Location(225, 475)); // Westbrook Garrison
+            myLocations.Add(new Location(315, 560)); // Stonefield Farm
+            myLocations.Add(new Location(315, 300)); // Stormwind Gates
+            myLocations.Add(new Location(400, 412)); // Goldshire
+            myLocations.Add(new Location(635, 450)); // Tower of Azora
+            myLocations.Add(new Location(400, 570)); // Maclure Vineyards
+
+
+            addStreet(myLocations[4], myLocations[3]);
+            addStreet(myLocations[1], myLocations[3]);
+            addStreet(myLocations[1], myLocations[5]);
+            addStreet(myLocations[1], myLocations[0]);
+            addStreet(myLocations[0], myLocations[3]);
+            addStreet(myLocations[2], myLocations[3]);
+
             Console.WriteLine("test started");
-            MapPage map = new MapPage();
-            DijkstraAlg dAlg = new DijkstraAlg(map.myLocations, map.myStreets);
+            /*DijkstraAlg dAlg = new DijkstraAlg(map.myLocations, map.myStreets);
             /*
             List<Street> testStreet = new List<Street>();
             testStreet.Add(addStreet(map.myLocations[1], map.myLocations[0])); // Westbrook Garrison to Stonefield farm. 
