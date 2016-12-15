bea7923239f43098e8caa2e5b68d81cec670b78a
LJ-S-H <lovejitshari@gmail.com>
2016-12-14 20:46:22 -0800

    commented about page

--- MiddleOut/AboutPage.xaml.cs
+++ MiddleOut/AboutPage.xaml.cs
@@ -16,8 +16,8 @@ using System.Windows.Shapes;
 namespace MiddleOut
 {
     /// <summary>
-    /// Co-Author: Lovejit Hari, worked on .xaml
-    /// Co-Author: Charlton Smith, worked on .xaml
+    /// @Co-Author: Lovejit Hari, worked on .xaml
+    /// @Co-Author: Charlton Smith, worked on .xaml
     /// This initializes the about page, most of the work is done in the .xaml code
     /// </summary>
     public partial class BasicPage2 : UserControl
